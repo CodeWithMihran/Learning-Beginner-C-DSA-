8c10bddea0dfd94aff2785ed5e624bbb46fedacb
CodeWithMihran <sohail.mihran@gmail.com>
2025-07-18 23:14:21 +0530

    Practiced Leetcode Questions

--- practice.cpp
+++ practice.cpp
@@ -1357,29 +1357,91 @@
 
 //PRODUCT OF ARRAY EXCEPT ITSELF 
 
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+
+// vector<int> productExceptSelf(vector<int>& nums){
+//         int n = nums.size();
+//     vector<int> ans(n,1);
+//     for(int i=0; i<n; i++){
+//         for(int j=0; j<n; j++){
+//             if(j != i){
+//                 ans[i] *= nums[j];
+//             }
+//         }
+//     }
+//     return ans;
+// }
+
+// int main(){
+//     vector<int> nums = {1,2,3,4};
+//     int n = nums.size();
+//     vector<int> ans = productExceptSelf(nums);
+//     for(int i=0; i<n; i++){
+//         cout<<ans[i]<<"  ";
+//     }
+//     return 0;
+// }
+
+//PRODUCT OF ARRAY EXCEPT ITSELF (PREFIX AND SUFFIX METHOD)
+
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+
+// vector<int> productExceptSelf(vector<int>& nums){
+//     int n = nums.size();
+//     vector<int> prefix(n,1);
+//     for(int i=1; i<n; i++){
+//         prefix[i] = prefix[i-1]*nums[i-1];
+//     }
+//     vector<int> suffix(n,1);
+//     suffix[n-1]=1;
+//     for(int i=n-2; i>=0; i--){
+//         suffix[i] = suffix[i+1]*nums[i+1];
+//     }
+//     vector<int> ans(n,1);
+//     for(int i=0; i<n; i++){
+//         ans[i]= suffix[i]*prefix[i];
+//     }
+//     return ans;
+// }
+
+// int main(){
+//     vector<int> nums = {1,2,3,4};
+//     vector<int> ans = productExceptSelf(nums);
+//     for(int i=0; i<nums.size(); i++){
+//         cout<< ans[i]<<" ";
+//     }
+//     return 0;
+// }
+
+//PRODUCT OF ARRAY EXCEPT ITSELF (PREFIX AND SUFFIX METHOD) SPACE COMPLEXITY = O(1)
+
 #include<iostream>
 #include<vector>
 using namespace std;
 
 vector<int> productExceptSelf(vector<int>& nums){
-        int n = nums.size();
+    int n = nums.size();
     vector<int> ans(n,1);
-    for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
-            if(j != i){
-                ans[i] *= nums[j];
-            }
-        }
+    for(int i=1; i<n; i++){
+        ans[i] = ans[i-1]*nums[i-1];
+    }
+    int suffix = 1;
+    for(int i=n-2; i>=0; i--){
+        suffix *= nums[i+1];
+        ans[i] *= suffix;
     }
     return ans;
 }
 
 int main(){
     vector<int> nums = {1,2,3,4};
-    int n = nums.size();
     vector<int> ans = productExceptSelf(nums);
-    for(int i=0; i<n; i++){
-        cout<<ans[i]<<"  ";
+    for(int i=0; i<nums.size(); i++){
+        cout<< ans[i]<<" ";
     }
     return 0;
 }
