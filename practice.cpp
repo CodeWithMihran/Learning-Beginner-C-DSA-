5b92b6258be2e6ca5588c9917aba95fa7305de52
CodeWithMihran <sohail.mihran@gmail.com>
2025-07-20 00:14:15 +0530

    Practised Leetcode Questions

--- practice.cpp
+++ practice.cpp
@@ -1514,41 +1514,71 @@
 
 // Modified Form of Binary Search (Rotated Sort Array)
 
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+
+// int search(vector<int>& nums, int tar){
+//     int n = nums.size();
+//     int st = 0, end = n-1;
+//     while(st <= end){
+//         int mid = st + (end-st)/2;
+//         if(nums[mid] == tar){
+//             return mid;
+//         }
+//         if(nums[st] <= nums[mid]){
+//             if(nums[st] <= tar && tar <= nums[mid]){
+//                 end = mid-1;
+//             }
+//             else {
+//                 st = mid+1;
+//             }
+//         }
+//         else{
+//             if(nums[mid] <= tar && tar <= nums[end]){
+//                 st = mid+1;
+//             }
+//             else {
+//                 end = mid-1;
+//             }
+//         }
+//     }
+//     return -1;
+// }
+
+// int main(){
+//     vector<int> nums = {3,4,5,6,7,0,1,2};
+//     int target = 0;
+//     cout<< "Your target is at index : "<<search(nums,target)<<endl;
+//     return 0;
+// }
+
+// Peak Index in Mountain Array (Binary Search)
+
 #include<iostream>
 #include<vector>
 using namespace std;
 
-int search(vector<int>& nums, int tar){
+int peakMountainSearch(vector<int> nums){
     int n = nums.size();
-    int st = 0, end = n-1;
+    int st = 1, end = n-2;
     while(st <= end){
         int mid = st + (end-st)/2;
-        if(nums[mid] == tar){
-            return mid;
+        if (nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1]){
+            return nums[mid];
         }
-        if(nums[st] <= nums[mid]){
-            if(nums[st] <= tar && tar <= nums[mid]){
-                end = mid-1;
-            }
-            else {
-                st = mid+1;
-            }
+        else if(nums[mid] > nums[mid-1]){
+            st = mid+1;
         }
         else{
-            if(nums[mid] <= tar && tar <= nums[end]){
-                st = mid+1;
-            }
-            else {
-                end = mid-1;
-            }
+            end = mid-1;
         }
     }
     return -1;
 }
 
 int main(){
-    vector<int> nums = {3,4,5,6,7,0,1,2};
-    int target = 0;
-    cout<< "Your target is at index : "<<search(nums,target)<<endl;
+    vector<int> nums = {0,3,8,5,2};
+    cout << "The Peak Value is : "<<peakMountainSearch(nums)<<endl;
     return 0;
 }
