56e0625524702eebc06bb56075ff70d39b073b36
CodeWithMihran <sohail.mihran@gmail.com>
2025-07-20 01:33:53 +0530

    Practised Leetcode Questions

--- practice.cpp
+++ practice.cpp
@@ -1555,30 +1555,79 @@
 
 // Peak Index in Mountain Array (Binary Search)
 
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+
+// int peakMountainSearch(vector<int> nums){
+//     int n = nums.size();
+//     int st = 1, end = n-2;
+//     while(st <= end){
+//         int mid = st + (end-st)/2;
+//         if (nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1]){
+//             return nums[mid];
+//         }
+//         else if(nums[mid] > nums[mid-1]){
+//             st = mid+1;
+//         }
+//         else{
+//             end = mid-1;
+//         }
+//     }
+//     return -1;
+// }
+
+// int main(){
+//     vector<int> nums = {0,3,8,5,2};
+//     cout << "The Peak Value is : "<<peakMountainSearch(nums)<<endl;
+//     return 0;
+// }
+
+// Single Element In Sorted Array
+
 #include<iostream>
 #include<vector>
 using namespace std;
 
-int peakMountainSearch(vector<int> nums){
+int singleElemSearch(vector<int> nums){
     int n = nums.size();
-    int st = 1, end = n-2;
+    int st= 0, end = n-1;
+    if(n == 1){
+        return nums[0];
+    }
     while(st <= end){
         int mid = st + (end-st)/2;
-        if (nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1]){
+        if(mid == 0 && nums[mid] != nums[mid+1]){
             return nums[mid];
         }
-        else if(nums[mid] > nums[mid-1]){
+        if(mid == (n-1) && nums[mid] != nums[mid-1]){
+            return nums[mid];
+        }
+        if(nums[mid] != nums[mid-1] && nums[mid] != nums[mid+1]){
+            return nums[mid];
+        }
+        if(mid % 2 == 0){
+        if(nums[mid] == nums[mid-1]){
+            end = mid-1;
+        }
+        else{
+            st = mid+1;
+        }
+    }
+    else{
+        if(nums[mid] == nums[mid-1]){
             st = mid+1;
         }
         else{
             end = mid-1;
         }
     }
+    }
     return -1;
 }
 
 int main(){
-    vector<int> nums = {0,3,8,5,2};
-    cout << "The Peak Value is : "<<peakMountainSearch(nums)<<endl;
+    vector<int> nums = {1,2,2,3,3,4,4,8,8};
+    cout<<"The Single Element in the Array is : "<<singleElemSearch(nums)<<endl;
     return 0;
 }
