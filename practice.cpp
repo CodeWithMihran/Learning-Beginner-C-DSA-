7f465029459e3719ddde296d2c7b75798b446910
CodeWithMihran <sohail.mihran@gmail.com>
2025-07-28 23:54:00 +0530

    Learning DSA Concepts (Strings)

--- practice.cpp
+++ practice.cpp
@@ -2097,40 +2097,61 @@
 
 // Valid Palindrome
 
+// #include<iostream>
+// #include<string>
+// using namespace std;
+
+// bool isAlphaNum(char ch){
+//     if((ch >= 0 && ch<=9) ||
+//         (tolower(ch) >= 'a' && tolower(ch) <= 'z')){
+//             return true;
+//         }
+//     return false;
+// }
+
+// bool isPalindrome(string s){
+//     int st = 0, end = s.size()-1;
+//     while(st<end){
+//         if(!isAlphaNum(s[st])){
+//             st++;   continue;
+//         }
+//         if(!isAlphaNum(s[end])){
+//             end--;  continue;
+//         }
+//         if(tolower(s[st]) != tolower(s[end])){
+//             return false;
+//         }
+//         else{
+//             st++;
+//             end--;
+//         }
+//     }
+//     return true;
+// }
+
+// int main(){
+//     string s = "Racecar";
+//     cout<<isPalindrome(s)<<endl;
+//     return 0;
+// }
+
+// Remove All Occurrences of a Substring
+
 #include<iostream>
 #include<string>
 using namespace std;
 
-bool isAlphaNum(char ch){
-    if((ch >= 0 && ch<=9) ||
-        (tolower(ch) >= 'a' && tolower(ch) <= 'z')){
-            return true;
-        }
-    return false;
-}
-
-bool isPalindrome(string s){
-    int st = 0, end = s.size()-1;
-    while(st<end){
-        if(!isAlphaNum(s[st])){
-            st++;   continue;
-        }
-        if(!isAlphaNum(s[end])){
-            end--;  continue;
-        }
-        if(tolower(s[st]) != tolower(s[end])){
-            return false;
-        }
-        else{
-            st++;
-            end--;
-        }
+string removeOccurences(string s, string part){
+    while (s.length()>0 && s.find(part) < s.length()){
+        s.erase(s.find(part), part.length());
     }
-    return true;
+    return s;
 }
 
 int main(){
-    string s = "Racecar";
-    cout<<isPalindrome(s)<<endl;
+    string s = "daabcbaabcbc";
+    string part = "abc";
+    string ans = removeOccurences(s,part);
+    cout<<"String after removing Occurences : "<<ans<<endl;
     return 0;
 }
