5ce87e0ddda892b21e4ea30db576e7e295530e82
CodeWithMihran <sohail.mihran@gmail.com>
2025-08-01 22:21:56 +0530

    Practiced Leetcode Questions

--- practice.cpp
+++ practice.cpp
@@ -2785,24 +2785,75 @@
 //     return 0;
 // }
 
+// Search in 2D Array (Binary Search) O(m*log(n))
+
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+
+// bool searchMatrix(vector<vector<int>> matrix, int target){
+//     for(int i=0; i<matrix.size(); i++){
+//         int st = 0, end = matrix[i].size()-1;
+//         while(st <= end){
+//             int mid = st + (end-st)/2;
+//             if(matrix[i][mid] == target){
+//                 return true;
+//             }
+//             if(matrix[i][mid] > target){
+//                 end = mid-1;
+//             }
+//             else{
+//                 st = mid+1;
+//             }
+//         }
+//     }
+//     return false;
+// }
+
+// int main(){
+//     vector<vector<int>> matrix = {{1,3,5,7},{10,11,16,20},{20,30,34,60}};
+//     int target = 100;
+//     cout<<searchMatrix(matrix ,target)<<endl;
+//     return 0;
+// }
+
+// More Optimised Solution O(log(m*n))
+
 #include<iostream>
 #include<vector>
 using namespace std;
 
+bool searchinRow(vector<vector<int>> matrix, int target, int row){
+    int n = matrix[0].size();
+    int st = 0, end = n-1;
+    while(st <= end){
+        int mid = st + (end-st)/2;
+        if(target == matrix[row][mid]){
+            return true;
+        }
+        if(target > matrix[row][mid]){
+            st = mid+1;
+        }
+        else{
+            end = mid-1;
+        }
+    }
+    return false;
+}
+
 bool searchMatrix(vector<vector<int>> matrix, int target){
-    for(int i=0; i<matrix.size(); i++){
-        int st = 0, end = matrix[i].size()-1;
-        while(st <= end){
-            int mid = st + (end-st)/2;
-            if(matrix[i][mid] == target){
-                return true;
-            }
-            if(matrix[i][mid] > target){
-                end = mid-1;
-            }
-            else{
-                st = mid+1;
-            }
+    int m = matrix.size(), n = matrix[0].size();
+    int stRow = 0 , endRow = m-1;
+    while(stRow <= endRow){
+        int midRow = stRow + (endRow-stRow)/2;
+        if(target >= matrix[midRow][0] && target <= matrix[midRow][n-1]){
+            return searchinRow(matrix,target,midRow);
+        }
+        if(target < matrix[midRow][0]){
+            endRow = midRow-1;
+        }
+        else{
+            stRow = midRow+1;
         }
     }
     return false;
@@ -2810,7 +2861,7 @@ bool searchMatrix(vector<vector<int>> matrix, int target){
 
 int main(){
     vector<vector<int>> matrix = {{1,3,5,7},{10,11,16,20},{20,30,34,60}};
-    int target = 100;
+    int target = 34;
     cout<<searchMatrix(matrix ,target)<<endl;
     return 0;
 }
