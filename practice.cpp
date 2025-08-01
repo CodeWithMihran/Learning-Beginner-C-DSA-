ef88034b3666e085eb4922d0a04c12f585e7244f
CodeWithMihran <sohail.mihran@gmail.com>
2025-08-02 00:05:37 +0530

    Practiced Leetcode Questions

--- practice.cpp
+++ practice.cpp
@@ -2819,49 +2819,122 @@
 
 // More Optimised Solution O(log(m*n))
 
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+
+// bool searchinRow(vector<vector<int>> matrix, int target, int row){
+//     int n = matrix[0].size();
+//     int st = 0, end = n-1;
+//     while(st <= end){
+//         int mid = st + (end-st)/2;
+//         if(target == matrix[row][mid]){
+//             return true;
+//         }
+//         if(target > matrix[row][mid]){
+//             st = mid+1;
+//         }
+//         else{
+//             end = mid-1;
+//         }
+//     }
+//     return false;
+// }
+
+// bool searchMatrix(vector<vector<int>> matrix, int target){
+//     int m = matrix.size(), n = matrix[0].size();
+//     int stRow = 0 , endRow = m-1;
+//     while(stRow <= endRow){
+//         int midRow = stRow + (endRow-stRow)/2;
+//         if(target >= matrix[midRow][0] && target <= matrix[midRow][n-1]){
+//             return searchinRow(matrix,target,midRow);
+//         }
+//         if(target < matrix[midRow][0]){
+//             endRow = midRow-1;
+//         }
+//         else{
+//             stRow = midRow+1;
+//         }
+//     }
+//     return false;
+// }
+
+// int main(){
+//     vector<vector<int>> matrix = {{1,3,5,7},{10,11,16,20},{20,30,34,60}};
+//     int target = 34;
+//     cout<<searchMatrix(matrix ,target)<<endl;
+//     return 0;
+// }
+
+// Search in a 2D Matrix II
+
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+
+// bool searchMatrix(vector<vector<int>> matrix, int target ){
+//     int m = matrix.size(), n = matrix[0].size();
+//     int row = 0, col = n-1;
+//     while(row < m && col >= 0){
+//         if(target == matrix[row][col]){
+//             return true;
+//         }
+//         else if(target > matrix[row][col]){
+//             row++;
+//         }
+//         else{
+//             col--;
+//         }
+//     }
+//     return false;
+// }
+
+// int main(){
+//     vector<vector<int>> matrix = {{1,4,7,11,15},{2,5,8,12,19},{3,6,9,16,22},{10,13,14,17,24},{18,21,23,26,30}};
+//     int target = 26;
+//     cout<<searchMatrix(matrix, target)<<endl;
+//     return 0;
+// }
+
 #include<iostream>
 #include<vector>
 using namespace std;
 
-bool searchinRow(vector<vector<int>> matrix, int target, int row){
-    int n = matrix[0].size();
-    int st = 0, end = n-1;
-    while(st <= end){
-        int mid = st + (end-st)/2;
-        if(target == matrix[row][mid]){
-            return true;
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        int m = matrix.size(), n=matrix[0].size();
+        int srow = 0, scol = 0, ecol = n-1, erow = m-1;
+        vector<int> ans;
+        while(srow <= erow && scol <= ecol){
+            for(int j=scol; j<= ecol; j++){
+                ans.push_back(matrix[srow][j]);
+            }
+            for(int i=srow+1; i<=erow; i++){
+                ans.push_back(matrix[i][ecol]);
+            }
+            for(int j=ecol-1; j>=scol; j--){
+                if(srow == erow){
+                    break;
+                }
+                ans.push_back(matrix[erow][j]);
+            }
+            for(int i=erow-1; i>=srow+1; i--){
+                if(scol == ecol){
+                    break;
+                }
+                ans.push_back(matrix[i][scol]);
+            }
+            srow++;  scol++; erow--; ecol--;
         }
-        if(target > matrix[row][mid]){
-            st = mid+1;
-        }
-        else{
-            end = mid-1;
-        }
+        return ans;
     }
-    return false;
-}
-
-bool searchMatrix(vector<vector<int>> matrix, int target){
-    int m = matrix.size(), n = matrix[0].size();
-    int stRow = 0 , endRow = m-1;
-    while(stRow <= endRow){
-        int midRow = stRow + (endRow-stRow)/2;
-        if(target >= matrix[midRow][0] && target <= matrix[midRow][n-1]){
-            return searchinRow(matrix,target,midRow);
-        }
-        if(target < matrix[midRow][0]){
-            endRow = midRow-1;
-        }
-        else{
-            stRow = midRow+1;
-        }
-    }
-    return false;
-}
 
 int main(){
-    vector<vector<int>> matrix = {{1,3,5,7},{10,11,16,20},{20,30,34,60}};
-    int target = 34;
-    cout<<searchMatrix(matrix ,target)<<endl;
+    vector<vector<int>> matrix = {{1,2,3},{4,5,6},{7,8,9}};
+    vector<int> spiral_matrix = spiralOrder(matrix);
+    cout<<"Matrix in spiral order : ";
+    for(int i=0; i<spiral_matrix.size(); i++){
+        cout << spiral_matrix[i]<<" ";
+    }
+    cout<<endl;
     return 0;
 }
