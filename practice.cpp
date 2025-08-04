4cffc82fffe2e1417450f74d1fe915eae9f5d44e
CodeWithMihran <sohail.mihran@gmail.com>
2025-08-04 22:21:11 +0530

    Practiced Leetcode Questions (Subarray Sum Equals K[O(n] Approach)

--- practice.cpp
+++ practice.cpp
@@ -3268,27 +3268,65 @@
 
 // Subarray Sum Equals K.
 
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+
+// int countSubarray(vector<int> &nums, int k){
+//     int n = nums.size();
+//     int count = 0;
+//     for(int i=0; i<n; i++){
+//         int sum = 0;
+//         for(int j=i; j<n; j++){
+//             sum += nums[j];
+//             if(sum == k){
+//                 count++;
+//             }
+//         }
+//     }
+//     return count;
+// }
+
+// int main(){
+//     vector<int> nums = {9,4,20,3,10,5};
+//     int k = 33;
+//     cout<<"The count of subarrays which sums upto k : "<<countSubarray(nums,k)<<endl;
+//     return 0;
+// }
+
 #include<iostream>
 #include<vector>
+#include<unordered_map>
 using namespace std;
 
-int countSubarray(vector<int> &nums, int k){
-    int n = nums.size();
-    int count = 0;
-    for(int i=0; i<n; i++){
-        int sum = 0;
-        for(int j=i; j<n; j++){
-            sum += nums[j];
-            if(sum == k){
+int countSubarray(vector<int>& nums, int k) {
+        int n = nums.size();
+        int count = 0;
+        vector<int> prefixSum(n,0);
+        prefixSum[0] = nums[0];
+        for(int i=1; i<n; i++){
+            prefixSum[i] = prefixSum[i-1] + nums[i];
+        }
+        unordered_map<int,int> m;
+        for(int j=0; j<n; j++){
+            if(prefixSum[j] == k){
                 count++;
             }
+
+            int val = prefixSum[j] - k;
+            if(m.find(val) != m.end()){
+                count += m[val];
+            }
+            if(m.find(prefixSum[j]) == m.end()){
+                m[prefixSum[j]] = 0;
+            }
+            m[prefixSum[j]]++;
         }
+        return count;
     }
-    return count;
-}
 
 int main(){
-    vector<int> nums = {9,4,20,3,10,5};
+    vector<int> nums = {9,4,0,20,3,10,5};
     int k = 33;
     cout<<"The count of subarrays which sums upto k : "<<countSubarray(nums,k)<<endl;
     return 0;
