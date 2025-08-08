7255f11acc79d8512a8cabb2b7c1621490637a6e
CodeWithMihran <sohail.mihran@gmail.com>
2025-08-08 22:43:32 +0530

    Just Checking Something

--- practice.cpp
+++ practice.cpp
@@ -3580,45 +3580,46 @@
 
 //Print and store all subsets without duplicate with recursion
 
-#include<iostream>
-#include<vector>
-#include<algorithm>
-using namespace std;
+// #include<iostream>
+// #include<vector>
+// #include<algorithm>
+// using namespace std;
 
-void getAllSubsets(vector<int>& nums, vector<int>& ans, int i, vector<vector<int>>& allSubsets){
-        if(i == nums.size()){
-            allSubsets.push_back(ans);
-            return;
-        }
-        ans.push_back(nums[i]);
-        getAllSubsets(nums, ans, i+1, allSubsets);
-        ans.pop_back();
-        int idx = i+1;
-        while(idx < nums.size() && nums[idx] == nums[idx-1]){
-            idx++;
-        }
-        getAllSubsets(nums, ans, idx, allSubsets);
-    }
+// void getAllSubsets(vector<int>& nums, vector<int>& ans, int i, vector<vector<int>>& allSubsets){
+//         if(i == nums.size()){
+//             allSubsets.push_back(ans);
+//             return;
+//         }
+//         ans.push_back(nums[i]);
+//         getAllSubsets(nums, ans, i+1, allSubsets);
+//         ans.pop_back();
+//         int idx = i+1;
+//         while(idx < nums.size() && nums[idx] == nums[idx-1]){
+//             idx++;
+//         }
+//         getAllSubsets(nums, ans, idx, allSubsets);
+//     }
 
-    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        vector<vector<int>> allSubsets;
-        vector<int> ans;
+//     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+//         sort(nums.begin(), nums.end());
+//         vector<vector<int>> allSubsets;
+//         vector<int> ans;
 
-        getAllSubsets(nums, ans, 0, allSubsets);
-        return allSubsets;
-    }
+//         getAllSubsets(nums, ans, 0, allSubsets);
+//         return allSubsets;
+//     }
 
-    int main(){
-    vector<int> nums = {1,2,2};
-    vector<vector<int>> allSubsets = subsetsWithDup(nums);
-     cout << "All subsets:" << endl;
-    for(auto subset : allSubsets){
-        cout << "{ ";
-        for(int val : subset){
-            cout << val << " ";
-        }
-        cout << "}" << endl;
-    }
-    return 0;
-}
+//     int main(){
+//     vector<int> nums = {1,2,2};
+//     vector<vector<int>> allSubsets = subsetsWithDup(nums);
+//      cout << "All subsets:" << endl;
+//     for(auto subset : allSubsets){
+//         cout << "{ ";
+//         for(int val : subset){
+//             cout << val << " ";
+//         }
+//         cout << "}" << endl;
+//     }
+//     return 0;
+// }
+
