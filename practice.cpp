fc0cdf9d8b64be1c2426f33e13a842b767a0a965
CodeWithMihran <sohail.mihran@gmail.com>
2025-08-09 23:46:45 +0530

    Just Checking Something

--- practice.cpp
+++ practice.cpp
@@ -3625,39 +3625,39 @@
 
 // Permutations by recursion
 
-#include<iostream>
-#include<vector>
-using namespace std;
+// #include<iostream>
+// #include<vector>
+// using namespace std;
 
-void getPerms(vector<int>& nums, int idx, vector<vector<int>>& ans){
-        int n = nums.size();
-        if(idx == n){
-            ans.push_back(nums);
-            return;
-        }
-        for(int i=idx; i<n; i++){
-            swap(nums[idx],nums[i]);
-            getPerms(nums, idx+1, ans);
-            swap(nums[idx],nums[i]);
-        }
-    }
+// void getPerms(vector<int>& nums, int idx, vector<vector<int>>& ans){
+//         int n = nums.size();
+//         if(idx == n){
+//             ans.push_back(nums);
+//             return;
+//         }
+//         for(int i=idx; i<n; i++){
+//             swap(nums[idx],nums[i]);
+//             getPerms(nums, idx+1, ans);
+//             swap(nums[idx],nums[i]);
+//         }
+//     }
 
-    vector<vector<int>> permute(vector<int>& nums) {
-        vector<vector<int>> ans;
-        getPerms(nums, 0, ans);
-        return ans;
-    }
+//     vector<vector<int>> permute(vector<int>& nums) {
+//         vector<vector<int>> ans;
+//         getPerms(nums, 0, ans);
+//         return ans;
+//     }
 
-int main(){
-    vector<int> nums = {1,2,3};
-    vector<vector<int>> ans = permute(nums);
-    cout << "All permutations :" << endl;
-    for(auto permutations : ans){
-        cout << "{ ";
-        for(int val : permutations){
-            cout << val << " ";
-        }
-        cout << "}" << endl;
-    }
-    return 0;
-}    
+// int main(){
+//     vector<int> nums = {1,2,3};
+//     vector<vector<int>> ans = permute(nums);
+//     cout << "All permutations :" << endl;
+//     for(auto permutations : ans){
+//         cout << "{ ";
+//         for(int val : permutations){
+//             cout << val << " ";
+//         }
+//         cout << "}" << endl;
+//     }
+//     return 0;
+// }    
