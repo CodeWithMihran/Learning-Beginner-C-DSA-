dc1344dce802f2ed1197623f3b8fb397d06e50c1
CodeWithMihran <sohail.mihran@gmail.com>
2025-08-12 23:47:33 +0530

    Just Checking Something

--- practice.cpp
+++ practice.cpp
@@ -3843,48 +3843,49 @@
 
 // Combination Sum
 
-#include<iostream>
-#include<vector>
-#include<set>
-using namespace std;
+// #include<iostream>
+// #include<vector>
+// #include<set>
+// using namespace std;
 
-set<vector<int>> s;
-    void getAllComb(vector<int>& candidates, int idx, vector<int>& combin, vector<vector<int>>& ans,int target){
-        int n = candidates.size();
-        if(idx == n || target<0){
-            return;
-        }
-        if(target == 0){
-            if(s.find(combin) == s.end()){
-                ans.push_back({combin});
-                s.insert(combin);
-            }
-            return;
-        }
-        combin.push_back(candidates[idx]);
-        getAllComb(candidates, idx+1, combin, ans, target-candidates[idx]);
-        getAllComb(candidates, idx, combin, ans, target-candidates[idx]);
-        combin.pop_back();
-        getAllComb(candidates, idx+1, combin, ans, target);
-    }
+// set<vector<int>> s;
+//     void getAllComb(vector<int>& candidates, int idx, vector<int>& combin, vector<vector<int>>& ans,int target){
+//         int n = candidates.size();
+//         if(idx == n || target<0){
+//             return;
+//         }
+//         if(target == 0){
+//             if(s.find(combin) == s.end()){
+//                 ans.push_back({combin});
+//                 s.insert(combin);
+//             }
+//             return;
+//         }
+//         combin.push_back(candidates[idx]);
+//         getAllComb(candidates, idx+1, combin, ans, target-candidates[idx]);
+//         getAllComb(candidates, idx, combin, ans, target-candidates[idx]);
+//         combin.pop_back();
+//         getAllComb(candidates, idx+1, combin, ans, target);
+//     }
 
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<int> combin;
-        vector<vector<int>> ans;
-        getAllComb(candidates,0, combin, ans, target);
-        return ans;
-    }
+//     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+//         vector<int> combin;
+//         vector<vector<int>> ans;
+//         getAllComb(candidates,0, combin, ans, target);
+//         return ans;
+//     }
 
-int main(){
-    vector<int> candidates = {2,3,5};
-    int target = 8;
-    vector<vector<int>> ans = combinationSum(candidates, target);
-    for(auto val : ans){
-        cout<<"{";
-        for(int comb : val){
-            cout<<comb<<" ";
-        }
-        cout<<"}"<<endl;
-    }
-    return 0;
-}    
+// int main(){
+//     vector<int> candidates = {2,3,5};
+//     int target = 8;
+//     vector<vector<int>> ans = combinationSum(candidates, target);
+//     for(auto val : ans){
+//         cout<<"{";
+//         for(int comb : val){
+//             cout<<comb<<" ";
+//         }
+//         cout<<"}"<<endl;
+//     }
+//     return 0;
+// }    
+
