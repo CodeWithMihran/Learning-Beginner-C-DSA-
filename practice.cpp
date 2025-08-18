291008fe05b9668a9f46271a0103f2c884c65971
CodeWithMihran <sohail.mihran@gmail.com>
2025-08-18 12:36:31 +0530

    Practiced Leetcode Questions (Palindrome Partitioning)

--- practice.cpp
+++ practice.cpp
@@ -3889,3 +3889,52 @@
 //     return 0;
 // }    
 
+// Palindrome Partiotioning
+
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+bool isPalindrome(string s){
+    string s2 = s;
+    reverse(s2.begin(), s2.end());
+    return s == s2;
+}
+
+void getAllParts(string s, vector<string>& partitions, vector<vector<string>>& ans){
+    if(s.size() == 0){
+        ans.push_back({partitions});
+        return;
+    }
+
+    for(int i=0; i<s.size(); i++){
+        string part = s.substr(0, i+1);
+        if(isPalindrome(part)){
+            partitions.push_back(part);
+            getAllParts(s.substr(i+1), partitions, ans);
+            partitions.pop_back();
+        }
+    }
+}
+
+vector<vector<string>> partition (string s){
+    vector<vector<string>> ans;
+    vector<string> partitions;
+    getAllParts(s, partitions, ans);
+    return ans;
+}
+
+int main(){
+    string s = "aab";
+    vector<vector<string>> ans = partition(s);
+    for(auto val : ans){
+        cout<<"{";
+        for(auto str : val){
+            cout<<str<<" ";
+        }
+        cout<<"}"<<endl;
+    }
+    return 0;
+}
