d8dc8c9c5e44973230adf10fa6224125c718d0ee
CodeWithMihran <sohail.mihran@gmail.com>
2025-08-20 19:27:11 +0530

    Practiced DSA Concept (Quick Sort)

--- practice.cpp
+++ practice.cpp
@@ -3941,50 +3941,89 @@
 
 // Merge Sort
 
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+
+// void merge(vector<int>& nums, int st, int mid, int end){
+//     vector<int> temp;
+//     int i = st, j = mid+1;
+//     while(i <= mid && j <= end){
+//         if(nums[i] <= nums[j]){
+//             temp.push_back(nums[i]);
+//             i++;
+//         }
+//         else{
+//             temp.push_back(nums[j]);
+//             j++;
+//         }
+//     }
+//     while(i <= mid){
+//         temp.push_back(nums[i]);
+//         i++;
+//     }
+//     while(j <= end){
+//         temp.push_back(nums[j]);
+//         j++;
+//     }
+//     for(int idx=0; idx<temp.size(); idx++){
+//         nums[idx+st] = temp[idx];
+//     }
+// }
+
+// void mergeSort(vector<int>& nums, int st, int end){
+//     if(st < end){
+//         int mid = st + (end-st)/2;
+//         mergeSort(nums, st, mid);
+//         mergeSort(nums, mid+1, end);
+//         merge(nums, st, mid, end);
+//     }
+// }
+
+// int main(){
+//     vector<int> nums = {12,31,35,8,32,17};
+//     mergeSort(nums, 0, nums.size()-1);
+//     cout<<"{";
+//     for(auto val : nums){
+//         cout<<val<<" ";
+//     }
+//     cout<<"}"<<endl;
+//     return 0;
+// }
+
+// Quick Sort
+
 #include<iostream>
 #include<vector>
 using namespace std;
 
-void merge(vector<int>& nums, int st, int mid, int end){
-    vector<int> temp;
-    int i = st, j = mid+1;
-    while(i <= mid && j <= end){
-        if(nums[i] <= nums[j]){
-            temp.push_back(nums[i]);
-            i++;
-        }
-        else{
-            temp.push_back(nums[j]);
-            j++;
+int partition(vector<int>& nums, int st, int end){
+    int pivot = nums[end], idx = st-1;
+    for(int j=st; j<end; j++){
+        if(nums[j] <= pivot){
+            idx++;
+            swap(nums[j], nums[idx]);
         }
     }
-    while(i <= mid){
-        temp.push_back(nums[i]);
-        i++;
-    }
-    while(j <= end){
-        temp.push_back(nums[j]);
-        j++;
-    }
-    for(int idx=0; idx<temp.size(); idx++){
-        nums[idx+st] = temp[idx];
-    }
+    idx++;
+    swap(nums[idx], nums[end]);
+    return idx;
 }
 
-void mergeSort(vector<int>& nums, int st, int end){
+void quickSort(vector<int>& nums, int st, int end){
     if(st < end){
-        int mid = st + (end-st)/2;
-        mergeSort(nums, st, mid);
-        mergeSort(nums, mid+1, end);
-        merge(nums, st, mid, end);
+        int pivIdx = partition(nums, st, end);
+        quickSort(nums, st, pivIdx-1);
+        quickSort(nums, pivIdx+1, end);
     }
+
 }
 
 int main(){
-    vector<int> nums = {12,31,35,8,32,17};
-    mergeSort(nums, 0, nums.size()-1);
+    vector<int> nums = {5,2,6,4,1,3};
+    quickSort(nums, 0, nums.size()-1);
     cout<<"{";
-    for(auto val : nums){
+    for(int val : nums){
         cout<<val<<" ";
     }
     cout<<"}"<<endl;
