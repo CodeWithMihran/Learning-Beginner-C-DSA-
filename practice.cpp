5645f3ac3c65cc24587d90507d860d72a5cb95e1
CodeWithMihran <sohail.mihran@gmail.com>
2025-08-25 22:51:53 +0530

    Practiced Leetcode Questions (Reversed Linked List)

--- practice.cpp
+++ practice.cpp
@@ -4130,140 +4130,159 @@
 
 // Linked List
 
-#include<iostream>
-using namespace std;
+// #include<iostream>
+// using namespace std;
 
-class Node{
+// class Node{
+// public:
+//     int data;
+//     Node* next;
+
+//     Node(int val){
+//         data = val;
+//         next  = NULL;
+//     }
+// };
+
+// class List{
+//     Node* head;
+//     Node* tail;
+
+//     public:
+//     List(){
+//         head = tail = NULL;
+//     }
+
+//     void push_front (int val){
+//         Node* newNode = new Node(val); // Dynamic
+//         // Node newNode(val); // Static
+//         if(head == NULL){
+//             head = tail = newNode;
+//             return;
+//         }
+//         else{
+//             newNode ->next = head;
+//             head = newNode;
+//         }
+//     }
+
+//     void push_back(int val){
+//         Node* newNode = new Node(val);
+
+//         if(head == NULL){
+//             head = tail = newNode;
+//             return;
+//         }
+//         else{
+//             tail -> next = newNode;
+//             tail = newNode;
+//         }
+//     }
+
+//     void pop_front(){
+//         if(head == NULL){
+//             return;
+//         }
+//         else{
+//             Node* temp = head;
+//             head = head -> next;
+//             temp -> next = NULL;
+//             delete temp;
+//         }
+//     }
+
+//     void pop_back(){
+//         if(head == NULL){
+//             return;
+//         }
+//         else{
+//             Node* temp = head;
+//             while(temp -> next != tail){
+//                 temp = temp -> next; 
+//             }
+//             temp -> next = NULL;
+//             delete tail;
+//             tail = temp;
+//         }
+//     }
+
+//     void insert(int val, int pos){
+//         if(pos<0){
+//             return;
+//         }
+//         if(pos == 0){
+//             push_front(val);
+//         }
+//         Node* newNode = new Node(val);
+//         Node* temp = head;
+//         for(int i=0; i<pos-1; i++){
+//             if(temp == NULL){
+//                 return;
+//             }
+//             temp = temp -> next;
+//         }
+//         newNode -> next = temp -> next;
+//         temp -> next = newNode;
+//     }
+
+//     int search(int key){
+//         Node* temp = head;
+//         int idx = 0;
+//         while(temp != NULL){
+//             if(temp -> data == key){
+//                 return idx;
+//             }
+//             temp = temp->next;
+//             idx++;
+//         }
+//         return -1;
+//     }
+
+//     void printll(){
+//         Node* temp = head;
+//         while(temp != NULL){
+//             cout<< temp -> data <<" ";
+//             temp = temp -> next;
+//         }
+//     }
+// };
+
+// int main(){
+//     List ll;
+//     ll.push_front(1);
+//     ll.push_front(2);
+//     ll.push_front(3);
+//     ll.push_back(4);
+//     ll.pop_front();
+//     ll.pop_back();
+//     ll.printll();
+//     cout<<endl;
+//     List l2;
+//     l2.push_front(1);
+//     l2.push_front(2);
+//     l2.push_front(3);
+//     l2.insert(5,1);
+//     l2.printll();
+//     cout<<endl;
+//     cout<<l2.search(1)<<endl;
+//     return 0;
+// }
+
+// Reverse Linked List
+
+class Solution {
 public:
-    int data;
-    Node* next;
-
-    Node(int val){
-        data = val;
-        next  = NULL;
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+        ListNode* next = NULL;
+        while(curr != NULL){
+            next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        } 
+        return prev;
     }
 };
 
-class List{
-    Node* head;
-    Node* tail;
-
-    public:
-    List(){
-        head = tail = NULL;
-    }
-
-    void push_front (int val){
-        Node* newNode = new Node(val); // Dynamic
-        // Node newNode(val); // Static
-        if(head == NULL){
-            head = tail = newNode;
-            return;
-        }
-        else{
-            newNode ->next = head;
-            head = newNode;
-        }
-    }
-
-    void push_back(int val){
-        Node* newNode = new Node(val);
-
-        if(head == NULL){
-            head = tail = newNode;
-            return;
-        }
-        else{
-            tail -> next = newNode;
-            tail = newNode;
-        }
-    }
-
-    void pop_front(){
-        if(head == NULL){
-            return;
-        }
-        else{
-            Node* temp = head;
-            head = head -> next;
-            temp -> next = NULL;
-            delete temp;
-        }
-    }
-
-    void pop_back(){
-        if(head == NULL){
-            return;
-        }
-        else{
-            Node* temp = head;
-            while(temp -> next != tail){
-                temp = temp -> next; 
-            }
-            temp -> next = NULL;
-            delete tail;
-            tail = temp;
-        }
-    }
-
-    void insert(int val, int pos){
-        if(pos<0){
-            return;
-        }
-        if(pos == 0){
-            push_front(val);
-        }
-        Node* newNode = new Node(val);
-        Node* temp = head;
-        for(int i=0; i<pos-1; i++){
-            if(temp == NULL){
-                return;
-            }
-            temp = temp -> next;
-        }
-        newNode -> next = temp -> next;
-        temp -> next = newNode;
-    }
-
-    int search(int key){
-        Node* temp = head;
-        int idx = 0;
-        while(temp != NULL){
-            if(temp -> data == key){
-                return idx;
-            }
-            temp = temp->next;
-            idx++;
-        }
-        return -1;
-    }
-
-    void printll(){
-        Node* temp = head;
-        while(temp != NULL){
-            cout<< temp -> data <<" ";
-            temp = temp -> next;
-        }
-    }
-};
-
-int main(){
-    List ll;
-    ll.push_front(1);
-    ll.push_front(2);
-    ll.push_front(3);
-    ll.push_back(4);
-    ll.pop_front();
-    ll.pop_back();
-    ll.printll();
-    cout<<endl;
-    List l2;
-    l2.push_front(1);
-    l2.push_front(2);
-    l2.push_front(3);
-    l2.insert(5,1);
-    l2.printll();
-    cout<<endl;
-    cout<<l2.search(1)<<endl;
-    return 0;
-}
