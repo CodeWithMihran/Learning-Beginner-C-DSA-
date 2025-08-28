820771698a9050aea82159c97f549868fb4fbf99
CodeWithMihran <sohail.mihran@gmail.com>
2025-08-28 23:50:06 +0530

    Just Checking Something

--- practice.cpp
+++ practice.cpp
@@ -4532,118 +4532,119 @@
 
 // Circular Linked List
 
-#include<iostream>
-using namespace std;
+// #include<iostream>
+// using namespace std;
 
-class Node{
-public:
-    int data;
-    Node* next;
+// class Node{
+// public:
+//     int data;
+//     Node* next;
 
-    Node(int val){
-        data = val;
-        next = NULL;
-    }
-};
+//     Node(int val){
+//         data = val;
+//         next = NULL;
+//     }
+// };
 
-class CircularList{
-    Node* head;
-    Node* tail;
-public:    
+// class CircularList{
+//     Node* head;
+//     Node* tail;
+// public:    
 
-    CircularList(){
-        head = tail = NULL;
-    }
+//     CircularList(){
+//         head = tail = NULL;
+//     }
 
-    void insertAtHead(int val){
-        Node* newNode = new Node(val);
-        if(tail == NULL){
-            head = tail = newNode;
-            tail->next = head;
-        }
-        else{
-            newNode->next = head;
-            head = newNode;
-            tail->next = head;
-        }
-    }
+//     void insertAtHead(int val){
+//         Node* newNode = new Node(val);
+//         if(tail == NULL){
+//             head = tail = newNode;
+//             tail->next = head;
+//         }
+//         else{
+//             newNode->next = head;
+//             head = newNode;
+//             tail->next = head;
+//         }
+//     }
 
-    void insertAtTail(int val){
-        Node* newNode = new Node(val);
-        if(tail == NULL){
-            head = tail = newNode;
-            tail->next = head;
-        }
-        else{
-            newNode->next = head;
-            tail->next = newNode;
-            tail = newNode;
-        }
-    }
+//     void insertAtTail(int val){
+//         Node* newNode = new Node(val);
+//         if(tail == NULL){
+//             head = tail = newNode;
+//             tail->next = head;
+//         }
+//         else{
+//             newNode->next = head;
+//             tail->next = newNode;
+//             tail = newNode;
+//         }
+//     }
 
-    void deleteAtHead(){
-        if(tail == NULL){
-            return;
-        }
-        else if(head == tail){
-            delete head;
-            head->next = NULL;
-            tail->next = NULL;
-        }
-        else{
-            Node* temp = head;
-        head = head->next;
-        tail->next = head;
-        temp->next = NULL;
-        delete temp;
-        }
-    }
+//     void deleteAtHead(){
+//         if(tail == NULL){
+//             return;
+//         }
+//         else if(head == tail){
+//             delete head;
+//             head->next = NULL;
+//             tail->next = NULL;
+//         }
+//         else{
+//             Node* temp = head;
+//         head = head->next;
+//         tail->next = head;
+//         temp->next = NULL;
+//         delete temp;
+//         }
+//     }
 
-    void deleteAtTail(){
-        if(tail == NULL){
-            return;
-        }
-        else if(head == tail){
-            delete head;
-            head->next = NULL;
-            tail->next = NULL;
-        }
-        else{
-            Node* temp = tail;
-            Node* prev = head;
-            while(prev->next != tail){
-                prev = prev->next;
-            }
-            tail = prev;
-            tail->next = head;
-            temp->next = NULL;
-            delete temp;
-        }
-    }
+//     void deleteAtTail(){
+//         if(tail == NULL){
+//             return;
+//         }
+//         else if(head == tail){
+//             delete head;
+//             head->next = NULL;
+//             tail->next = NULL;
+//         }
+//         else{
+//             Node* temp = tail;
+//             Node* prev = head;
+//             while(prev->next != tail){
+//                 prev = prev->next;
+//             }
+//             tail = prev;
+//             tail->next = head;
+//             temp->next = NULL;
+//             delete temp;
+//         }
+//     }
 
-    void print(){
-        if(head == NULL){
-            return;
-        }
-        cout<<head->data<<" -> ";
-        Node* temp = head->next;
-        while(temp != head){
-            cout<<temp->data<<" -> ";
-            temp = temp->next;
-        }
-        cout<<temp->data;
-    }
-};
+//     void print(){
+//         if(head == NULL){
+//             return;
+//         }
+//         cout<<head->data<<" -> ";
+//         Node* temp = head->next;
+//         while(temp != head){
+//             cout<<temp->data<<" -> ";
+//             temp = temp->next;
+//         }
+//         cout<<temp->data;
+//     }
+// };
 
-int main(){
-    CircularList cll;
-    cll.insertAtHead(1);
-    cll.insertAtHead(2);
-    cll.insertAtHead(3);
-    cll.insertAtTail(4);
-    cll.insertAtTail(5);
-    cll.deleteAtHead();
-    cll.deleteAtTail();
-    cll.print();
-    return 0;
-}
+// int main(){
+//     CircularList cll;
+//     cll.insertAtHead(1);
+//     cll.insertAtHead(2);
+//     cll.insertAtHead(3);
+//     cll.insertAtTail(4);
+//     cll.insertAtTail(5);
+//     cll.deleteAtHead();
+//     cll.deleteAtTail();
+//     cll.print();
+//     return 0;
+// }
+
