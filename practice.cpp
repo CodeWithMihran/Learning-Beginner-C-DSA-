72bf0b598ef64c6ffdfb4827394a27a0273bff44
CodeWithMihran <sohail.mihran@gmail.com>
2025-08-28 22:08:39 +0530

    Learned Doubly Linked List Concept and Making it itself by OOPs Concept

--- practice.cpp
+++ practice.cpp
@@ -4402,31 +4402,130 @@
 
 // Copy List with Random Pointer
 
-class Solution {
+// class Solution {
+// public:
+//     Node* copyRandomList(Node* head) {
+//         if(head == NULL){
+//             return NULL;
+//         }
+//         unordered_map<Node*,Node*>m;
+//         Node* newHead = new Node(head->val);
+//         Node* oldTemp = head->next;
+//         Node* newTemp = newHead;
+//         m[head] = newHead;
+//         while(oldTemp != NULL){
+//             Node* copyNode = new Node(oldTemp->val);
+//             m[oldTemp] = copyNode;
+//             newTemp->next = copyNode;
+//             oldTemp = oldTemp->next;
+//             newTemp = newTemp->next;
+//         }
+//         oldTemp = head;
+//         newTemp = newHead;
+//         while(oldTemp != NULL){
+//             newTemp->random = m[oldTemp->random];
+//             oldTemp = oldTemp->next;
+//             newTemp = newTemp->next;
+//         }
+//         return newHead;
+//     }
+// };
+
+// Doubly Linked List
+
+#include<iostream>
+using namespace std;
+
+class Node{
 public:
-    Node* copyRandomList(Node* head) {
-        if(head == NULL){
-            return NULL;
-        }
-        unordered_map<Node*,Node*>m;
-        Node* newHead = new Node(head->val);
-        Node* oldTemp = head->next;
-        Node* newTemp = newHead;
-        m[head] = newHead;
-        while(oldTemp != NULL){
-            Node* copyNode = new Node(oldTemp->val);
-            m[oldTemp] = copyNode;
-            newTemp->next = copyNode;
-            oldTemp = oldTemp->next;
-            newTemp = newTemp->next;
-        }
-        oldTemp = head;
-        newTemp = newHead;
-        while(oldTemp != NULL){
-            newTemp->random = m[oldTemp->random];
-            oldTemp = oldTemp->next;
-            newTemp = newTemp->next;
-        }
-        return newHead;
+    int data;
+    Node* next;
+    Node* prev;
+
+    Node(int val){
+        data = val;
+        next = prev = NULL;
     }
 };
+
+class DoublyList{
+    Node* head;
+    Node* tail;
+  
+public:
+    DoublyList(){
+        head = tail = NULL;
+    }
+
+    void push_front(int val){
+        Node* newNode = new Node(val);
+        if(head == NULL){
+            head = tail = newNode;
+        }
+        else{
+            newNode->next = head;
+            head->prev = newNode;
+            head = newNode;
+        }
+    }
+
+    void push_back(int val){
+        Node* newNode = new Node(val);
+        if(head == NULL){
+            head = tail = newNode;
+        }
+        else{
+            newNode->prev = tail;
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+
+    void pop_front(){
+        if(head == NULL){
+            return;
+        }
+        Node* temp = head;
+        head = head->next;
+        if(head != NULL){
+            head->prev = NULL;
+        }
+        temp->next = NULL;
+        delete temp;
+    }
+
+    void pop_back(){
+        if(head == NULL){
+            return;
+        }
+        Node* temp = tail;
+        tail = tail->prev;
+        if(tail != NULL){
+            tail->next = NULL;
+        }
+        temp->prev = NULL;
+        delete temp;
+    }
+
+    void print(){
+        Node* temp = head;
+        while(temp != NULL){
+            cout<<temp->data<<" <=> ";
+            temp = temp->next;
+        }
+        cout<<"NULL\n";
+    }
+};
+
+int main(){
+    DoublyList dll;
+    dll.push_front(1);
+    dll.push_front(2);
+    dll.push_front(3);
+    dll.push_back(4);
+    dll.push_back(5);
+    dll.pop_back();
+    dll.pop_front();
+    dll.print();
+    return 0;
+}
