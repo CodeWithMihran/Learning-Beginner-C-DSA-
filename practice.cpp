8afcc1d2365fd2908f38eb9ce17e4d357bbe6480
CodeWithMihran <sohail.mihran@gmail.com>
2025-08-31 17:17:52 +0530

    Practiced Leetcode Questions (Next Greater Element - I)

--- practice.cpp
+++ practice.cpp
@@ -4909,33 +4909,74 @@
 
 // Next Greater Element
 
+// #include<iostream>
+// #include<stack>
+// #include<vector>
+// using namespace std;
+
+// vector<int> nextGreaterElm(vector<int>& arr){
+//     stack<int> s;
+//     vector<int> ans(arr.size(), 0);
+//     int n = arr.size();
+//     for(int i=n-1; i>=0; i--){
+//         while(s.size()>0 && s.top() <= arr[i]){
+//             s.pop();
+//         }
+//         if(s.empty()){
+//             ans[i] = -1;
+//         }
+//         else{
+//             ans[i] = s.top();
+//         }
+//         s.push(arr[i]);
+//     }
+//     return ans;
+// }
+
+// int main(){
+//     vector<int> arr = {6,8,0,1,3};
+//     vector<int> ans = nextGreaterElm(arr);
+//     for(int val : ans){
+//         cout<<val<<" ";
+//     }
+//     cout<<endl;
+//     return 0;
+// }
+
+// Next Greater Element - I
+
 #include<iostream>
 #include<stack>
 #include<vector>
+#include<unordered_map>
 using namespace std;
 
-vector<int> nextGreaterElm(vector<int>& arr){
-    stack<int> s;
-    vector<int> ans(arr.size(), 0);
-    int n = arr.size();
-    for(int i=n-1; i>=0; i--){
-        while(s.size()>0 && s.top() <= arr[i]){
-            s.pop();
+vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> ans;
+        stack<int> s;
+        unordered_map<int,int> m;
+        for(int i=nums2.size()-1; i>=0; i--){
+            while(s.size() > 0 && s.top() <= nums2[i]){
+                s.pop();
+            }
+            if(s.empty()){
+                m[nums2[i]] = -1;
+            }
+            else{
+                m[nums2[i]] = s.top();
+            }
+            s.push(nums2[i]);
         }
-        if(s.empty()){
-            ans[i] = -1;
+        for(int i=0; i<nums1.size(); i++){
+            ans.push_back(m[nums1[i]]);
         }
-        else{
-            ans[i] = s.top();
-        }
-        s.push(arr[i]);
+        return ans;
     }
-    return ans;
-}
 
-int main(){
-    vector<int> arr = {6,8,0,1,3};
-    vector<int> ans = nextGreaterElm(arr);
+    int main(){
+    vector<int> nums2 = {6,8,0,1,3};
+    vector<int> nums1 = {3,8,6,0};
+    vector<int> ans = nextGreaterElement(nums1, nums2);
     for(int val : ans){
         cout<<val<<" ";
     }
