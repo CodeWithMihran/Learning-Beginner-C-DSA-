d91305ff4260409c9ff02a57b1fc0b02d56de8cf
CodeWithMihran <sohail.mihran@gmail.com>
2025-08-31 17:48:30 +0530

    Learned how to find Previous Smaller Element using Stack

--- practice.cpp
+++ practice.cpp
@@ -4945,38 +4945,73 @@
 
 // Next Greater Element - I
 
+// #include<iostream>
+// #include<stack>
+// #include<vector>
+// #include<unordered_map>
+// using namespace std;
+
+// vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+//         vector<int> ans;
+//         stack<int> s;
+//         unordered_map<int,int> m;
+//         for(int i=nums2.size()-1; i>=0; i--){
+//             while(s.size() > 0 && s.top() <= nums2[i]){
+//                 s.pop();
+//             }
+//             if(s.empty()){
+//                 m[nums2[i]] = -1;
+//             }
+//             else{
+//                 m[nums2[i]] = s.top();
+//             }
+//             s.push(nums2[i]);
+//         }
+//         for(int i=0; i<nums1.size(); i++){
+//             ans.push_back(m[nums1[i]]);
+//         }
+//         return ans;
+//     }
+
+//     int main(){
+//     vector<int> nums2 = {6,8,0,1,3};
+//     vector<int> nums1 = {3,8,6,0};
+//     vector<int> ans = nextGreaterElement(nums1, nums2);
+//     for(int val : ans){
+//         cout<<val<<" ";
+//     }
+//     cout<<endl;
+//     return 0;
+// }
+
+// Previous Smaller Element
+
 #include<iostream>
-#include<stack>
 #include<vector>
-#include<unordered_map>
+#include<stack>
 using namespace std;
 
-vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> ans;
-        stack<int> s;
-        unordered_map<int,int> m;
-        for(int i=nums2.size()-1; i>=0; i--){
-            while(s.size() > 0 && s.top() <= nums2[i]){
-                s.pop();
-            }
-            if(s.empty()){
-                m[nums2[i]] = -1;
-            }
-            else{
-                m[nums2[i]] = s.top();
-            }
-            s.push(nums2[i]);
+vector<int> prevSmallerElement(vector<int>& arr){
+    vector<int> ans(arr.size(),0);
+    stack<int> s;
+    for(int i=0; i<arr.size(); i++){
+        while(s.size() > 0 && s.top() >= arr[i]){
+            s.pop();
         }
-        for(int i=0; i<nums1.size(); i++){
-            ans.push_back(m[nums1[i]]);
+        if(s.empty()){
+            ans[i] = -1;
         }
-        return ans;
+        else{
+            ans[i] = s.top();
+        }
+        s.push(arr[i]);
     }
+    return ans;
+}
 
-    int main(){
-    vector<int> nums2 = {6,8,0,1,3};
-    vector<int> nums1 = {3,8,6,0};
-    vector<int> ans = nextGreaterElement(nums1, nums2);
+int main(){
+    vector<int> arr = {3,1,0,8,6};
+    vector<int> ans = prevSmallerElement(arr);
     for(int val : ans){
         cout<<val<<" ";
     }
