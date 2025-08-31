1218a4d23024839e790746faab5ea2afdd4372b1
CodeWithMihran <sohail.mihran@gmail.com>
2025-08-31 17:15:10 +0530

    Learned how to find Next Greater Element using Stack

--- practice.cpp
+++ practice.cpp
@@ -4874,33 +4874,69 @@
 
 // Stock Span
 
+// #include<iostream>
+// #include<stack>
+// #include<vector>
+// using namespace std;
+
+// vector<int> stockSpan(vector<int>& price){
+//     vector<int> ans;
+//     stack<int> s;
+//     for(int i=0; i<price.size(); i++){
+//         while(s.size()>0 && price[s.top()] <= price[i]){
+//             s.pop();
+//         }
+//         if(s.size() == 0){
+//             ans.push_back(i+1);
+//         }
+//         else{
+//             ans.push_back(i-s.top());
+//         }
+//         s.push(i);
+//     }
+//     return ans;
+// }
+
+// int main(){
+//     vector<int> price = {100,80,60,70,60,75,85};
+//     vector<int>ans = stockSpan(price);
+//     for(auto val: ans){
+//         cout<<val<<" ";
+//     }
+//     cout<<endl;
+//     return 0;
+// }
+
+// Next Greater Element
+
 #include<iostream>
 #include<stack>
 #include<vector>
 using namespace std;
 
-vector<int> stockSpan(vector<int>& price){
-    vector<int> ans;
+vector<int> nextGreaterElm(vector<int>& arr){
     stack<int> s;
-    for(int i=0; i<price.size(); i++){
-        while(s.size()>0 && price[s.top()] <= price[i]){
+    vector<int> ans(arr.size(), 0);
+    int n = arr.size();
+    for(int i=n-1; i>=0; i--){
+        while(s.size()>0 && s.top() <= arr[i]){
             s.pop();
         }
-        if(s.size() == 0){
-            ans.push_back(i+1);
+        if(s.empty()){
+            ans[i] = -1;
         }
         else{
-            ans.push_back(i-s.top());
+            ans[i] = s.top();
         }
-        s.push(i);
+        s.push(arr[i]);
     }
     return ans;
 }
 
 int main(){
-    vector<int> price = {100,80,60,70,60,75,85};
-    vector<int>ans = stockSpan(price);
-    for(auto val: ans){
+    vector<int> arr = {6,8,0,1,3};
+    vector<int> ans = nextGreaterElm(arr);
+    for(int val : ans){
         cout<<val<<" ";
     }
     cout<<endl;
