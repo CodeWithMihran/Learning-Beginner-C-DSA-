c0d792143032c161d2b27578ff0bb2134a0109c9
CodeWithMihran <sohail.mihran@gmail.com>
2025-09-01 23:43:43 +0530

    Practiced Leetcode Questions (Trapping Rain Water)

--- practice.cpp
+++ practice.cpp
@@ -5181,31 +5181,62 @@
 
 // Next Greater Element - II
 
+// #include<iostream>
+// #include<vector>
+// #include<stack>
+// using namespace std;
+
+// vector<int> nextGreaterElements(vector<int>& nums) {
+//         int n = nums.size();
+//         vector<int> ans(n,-1);
+//         stack<int> s;
+//         for(int i=2*n-1; i>=0; i--){
+//             while(s.size()>0 && nums[s.top()] <= nums[i%n]){
+//                 s.pop();
+//             }
+//             ans[i%n] = s.empty()? -1 : nums[s.top()];
+//             s.push(i%n);
+//         }
+//         return ans;
+//     }
+
+// int main(){
+//     vector<int> nums = {1,2,3,4,3};
+//     vector<int> ans = nextGreaterElements(nums);
+//     for(int val : ans){
+//         cout<<val<<" ";
+//     }
+//     cout<<endl;
+//     return 0;
+// }
+
+// Trapping Rain Water
+
 #include<iostream>
 #include<vector>
-#include<stack>
 using namespace std;
 
-vector<int> nextGreaterElements(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> ans(n,-1);
-        stack<int> s;
-        for(int i=2*n-1; i>=0; i--){
-            while(s.size()>0 && nums[s.top()] <= nums[i%n]){
-                s.pop();
-            }
-            ans[i%n] = s.empty()? -1 : nums[s.top()];
-            s.push(i%n);
+int trap(vector<int>& height) {
+        int n = height.size();
+        vector<int> lmax(n,0);
+        vector<int> rmax(n,0);
+        lmax[0] = height[0];
+        rmax[n-1] = height[n-1];
+        for(int i=1; i<n; i++){
+            lmax[i] = max(lmax[i-1], height[i]);
+        }
+        for(int i=n-2; i>=0; i--){
+            rmax[i] = max(rmax[i+1], height[i]);
+        }
+        int ans = 0;
+        for(int i=0; i<n; i++){
+            ans += min(lmax[i], rmax[i]) - height[i];
         }
         return ans;
     }
 
 int main(){
-    vector<int> nums = {1,2,3,4,3};
-    vector<int> ans = nextGreaterElements(nums);
-    for(int val : ans){
-        cout<<val<<" ";
-    }
-    cout<<endl;
+    vector<int> height = {4,2,0,3,2,5};
+    cout<<trap(height)<<endl;
     return 0;
-}
+}    
