39e908860ae8c4116beaf27e435974a78d6bba49
CodeWithMihran <sohail.mihran@gmail.com>
2025-09-01 23:58:56 +0530

    Practiced Leetcode Questions (Trapping Rain Water [S.C.: O(1)])

--- practice.cpp
+++ practice.cpp
@@ -5210,7 +5210,38 @@
 //     return 0;
 // }
 
-// Trapping Rain Water
+// Trapping Rain Water (S.C. : O(n))
+
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+
+// int trap(vector<int>& height) {
+//         int n = height.size();
+//         vector<int> lmax(n,0);
+//         vector<int> rmax(n,0);
+//         lmax[0] = height[0];
+//         rmax[n-1] = height[n-1];
+//         for(int i=1; i<n; i++){
+//             lmax[i] = max(lmax[i-1], height[i]);
+//         }
+//         for(int i=n-2; i>=0; i--){
+//             rmax[i] = max(rmax[i+1], height[i]);
+//         }
+//         int ans = 0;
+//         for(int i=0; i<n; i++){
+//             ans += min(lmax[i], rmax[i]) - height[i];
+//         }
+//         return ans;
+//     }
+
+// Trapping Rain Water (S.C. : O(1))
+
+// int main(){
+//     vector<int> height = {4,2,0,3,2,5};
+//     cout<<trap(height)<<endl;
+//     return 0;
+// }    
 
 #include<iostream>
 #include<vector>
