3e46832457f70ed107095755fdf3997a6bfa8165
CodeWithMihran <sohail.mihran@gmail.com>
2025-09-03 23:06:00 +0530

    Learned Queue Concept by STL Library

--- practice.cpp
+++ practice.cpp
@@ -5405,63 +5405,81 @@
 
 // Queue Data Structure
 
+// #include<iostream>
+// #include<list>
+// using namespace std;
+
+// class Node{
+// public:    
+//     int data;
+//     Node* next;
+
+//     Node(int val){
+//         data = val;
+//         next = NULL;
+//     }
+// };
+
+// class Queue{
+//     Node* head;
+//     Node* tail;
+
+// public:
+//     Queue(){
+//         head = tail = NULL;
+
+//     }
+
+//     void push(int data){
+//         Node* newNode = new Node(data);
+//         if(head == NULL){
+//             head = tail = newNode;
+//         }
+//         else{
+//             tail->next = newNode;
+//             tail = newNode;
+//             tail->next =  NULL;
+//         }
+//     }
+
+//     void pop(){
+//         if(empty()){
+//             return;
+//         }
+//         Node* temp = head;
+//         head = head->next;
+//         delete temp;
+//     }
+
+//     int front(){
+//         return head->data;
+//     }
+
+//     bool empty(){
+//         return head == NULL;
+//     }
+// };
+
+// int main(){
+//     Queue q;
+//     q.push(1);
+//     q.push(2);
+//     q.push(3);
+
+//     while(!q.empty()){
+//         cout<<q.front()<<" ";
+//         q.pop();
+//     }
+//     cout<<endl;
+//     return 0;
+// }
+
 #include<iostream>
-#include<list>
+#include<queue>
 using namespace std;
 
-class Node{
-public:    
-    int data;
-    Node* next;
-
-    Node(int val){
-        data = val;
-        next = NULL;
-    }
-};
-
-class Queue{
-    Node* head;
-    Node* tail;
-
-public:
-    Queue(){
-        head = tail = NULL;
-
-    }
-
-    void push(int data){
-        Node* newNode = new Node(data);
-        if(head == NULL){
-            head = tail = newNode;
-        }
-        else{
-            tail->next = newNode;
-            tail = newNode;
-            tail->next =  NULL;
-        }
-    }
-
-    void pop(){
-        if(empty()){
-            return;
-        }
-        Node* temp = head;
-        head = head->next;
-        delete temp;
-    }
-
-    int front(){
-        return head->data;
-    }
-
-    bool empty(){
-        return head == NULL;
-    }
-};
-
 int main(){
-    Queue q;
+    queue<int> q;
     q.push(1);
     q.push(2);
     q.push(3);
