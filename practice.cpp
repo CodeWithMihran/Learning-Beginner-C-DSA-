58f01b1386e40d3fd244e087bf0e35a18514b7bf
CodeWithMihran <sohail.mihran@gmail.com>
2025-09-04 22:07:44 +0530

    Practiced Leetcode Questions (Implement Queue using Stack)

--- practice.cpp
+++ practice.cpp
@@ -5614,54 +5614,110 @@
 
 // Implement Stack using Queue
 
+// #include<iostream>
+// #include<queue>
+// using namespace std;
+
+
+// class MyStack {
+// public:
+//     queue<int> q1;
+//     queue<int> q2;
+//     MyStack() {
+        
+//     }
+    
+//     void push(int x) {
+//         while(!q1.empty()){
+//             q2.push(q1.front());
+//             q1.pop();
+//         }    
+//         q1.push(x);
+//         while(!q2.empty()){
+//             q1.push(q2.front());
+//             q2.pop();
+//         }
+//     }
+    
+//     int pop() {
+//         int ans = q1.front();
+//         q1.pop();
+//         return ans;
+//     }
+    
+//     int top() {
+//         return q1.front();
+//     }
+    
+//     bool empty() {
+//         return q1.empty();
+//     }
+// };
+
+// int main(){
+//     MyStack s;
+//     s.push(1);
+//     s.push(2);
+//     s.push(3);
+//     while(!s.empty()){
+//         cout<<s.top()<<" ";
+//         s.pop();
+//     }
+//     cout<<endl;
+//     return 0;
+// }
+
+
+// Implement Queue using Stack
+
 #include<iostream>
-#include<queue>
+#include<stack>
 using namespace std;
 
-
-class MyStack {
+class MyQueue {
 public:
-    queue<int> q1;
-    queue<int> q2;
-    MyStack() {
+    stack<int> s1;
+    stack<int> s2;
+    MyQueue() {
         
     }
     
     void push(int x) {
-        while(!q1.empty()){
-            q2.push(q1.front());
-            q1.pop();
-        }    
-        q1.push(x);
-        while(!q2.empty()){
-            q1.push(q2.front());
-            q2.pop();
+        while(!s1.empty()){
+            s2.push(s1.top());
+            s1.pop();
+        }
+        s1.push(x);
+        while(!s2.empty()){
+            s1.push(s2.top());
+            s2.pop();
         }
     }
     
     int pop() {
-        int ans = q1.front();
-        q1.pop();
+        int ans = s1.top();
+        s1.pop();
         return ans;
     }
     
-    int top() {
-        return q1.front();
+    int peek() {
+        return s1.top();
     }
     
     bool empty() {
-        return q1.empty();
+        return s1.empty();
     }
 };
 
 int main(){
-    MyStack s;
-    s.push(1);
-    s.push(2);
-    s.push(3);
-    while(!s.empty()){
-        cout<<s.top()<<" ";
-        s.pop();
+    MyQueue q;
+    q.push(1);
+    q.push(2);
+    q.push(3);
+
+    while(!q.empty()){
+        cout<<q.peek()<<" ";
+        q.pop();
     }
     cout<<endl;
     return 0;
