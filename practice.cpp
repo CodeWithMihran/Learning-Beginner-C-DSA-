10b3200eb638f7aa3da0b91872e6103b4450273f
CodeWithMihran <sohail.mihran@gmail.com>
2025-09-08 23:33:37 +0530

    Learned Binary Tree and Level Order Concept

--- practice.cpp
+++ practice.cpp
@@ -5827,6 +5827,7 @@
 
 #include<iostream>
 #include<vector>
+#include<queue>
 using namespace std;
 
 class Node{
@@ -5870,13 +5871,46 @@ void inOrder(Node* root){
     inOrder(root->right);
 }
 
+void postOrder(Node* root){
+    if(root == NULL){
+        return;
+    }
+    postOrder(root->left);
+    postOrder(root->right);
+    cout<<root->data<<" ";
+}
+
+void levelOrder(Node* root){
+    queue<Node*> q;
+    q.push(root);
+    q.push(NULL);
+    while(q.size() > 0){
+        Node* curr = q.front();
+        q.pop();
+        if(curr == NULL){
+            if(!q.empty()){
+                cout<<endl;
+                q.push(NULL);
+                continue;
+            }
+            else{
+                break;
+            }
+        }
+        cout<< curr->data<<" ";
+        if(curr->left != NULL){
+            q.push(curr->left);
+        }
+        if(curr->right != NULL){
+            q.push(curr->right);
+        }
+    }
+    cout<<endl;
+}
 int main(){
     vector<int> preorder = {1,2,-1,-1,3,4,-1,-1,5,-1,-1};
 
     Node* root = buildTree(preorder);
-    preOrder(root);
-    cout<<endl;
-    inOrder(root);
-    cout<<endl;
+    levelOrder(root);
     return 0;
 }
