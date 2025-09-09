ebf2ea5411dc8f092069bf72a72ca41b271c5557
CodeWithMihran <sohail.mihran@gmail.com>
2025-09-09 22:05:52 +0530

    Solved the Number of Nodes of the Tree Question

--- practice.cpp
+++ practice.cpp
@@ -5917,6 +5917,52 @@
 
 // Height of Tree
 
+// #include<iostream>
+// #include<vector>
+// #include<queue>
+// using namespace std;
+
+// class Node{
+// public:
+//     int data;
+//     Node* left;
+//     Node* right;
+
+//     Node(int val){
+//         data = val;
+//         left = right = NULL;
+//     }
+// };
+
+// static int idx = -1;
+// Node* buildTree(vector<int>& preorder){
+//     idx++;
+
+//     if(preorder[idx] == -1) return NULL;
+//     Node* root = new Node(preorder[idx]);
+//     root->left = buildTree(preorder);
+//     root->right = buildTree(preorder);
+//     return root;
+// }
+
+// int height(Node* root){
+//     if(root == NULL){
+//         return 0;
+//     }
+//     int leftHt = height(root->left);
+//     int rightHt = height(root->right);
+//     return max(leftHt, rightHt)+1;
+// }
+
+// int main(){
+//     vector<int> preorder = {1,2,-1,-1,3,4,-1,-1,5,-1,-1};
+//     Node* root = buildTree(preorder);
+//     cout<<"Height of the Tree : "<<height(root)<<endl;
+//     return 0;
+// }
+
+// Count of Nodes
+
 #include<iostream>
 #include<vector>
 #include<queue>
@@ -5945,18 +5991,19 @@ Node* buildTree(vector<int>& preorder){
     return root;
 }
 
-int height(Node* root){
+int count(Node* root){
     if(root == NULL){
         return 0;
     }
-    int leftHt = height(root->left);
-    int rightHt = height(root->right);
-    return max(leftHt, rightHt)+1;
+
+    int leftCt = count(root->left);
+    int rightCt = count(root->right);
+    return leftCt + rightCt +1;
 }
 
 int main(){
     vector<int> preorder = {1,2,-1,-1,3,4,-1,-1,5,-1,-1};
     Node* root = buildTree(preorder);
-    cout<<"Height of the Tree : "<<height(root)<<endl;
+    cout<<"Count of Nodes of the Tree : "<<count(root)<<endl;
     return 0;
 }
