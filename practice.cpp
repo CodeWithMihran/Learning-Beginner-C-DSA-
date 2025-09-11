fde959077d58e34282a0369c5a6aab7f59cd224c
CodeWithMihran <sohail.mihran@gmail.com>
2025-09-11 23:47:08 +0530

    Practiced Leetcode Questions (Top View of a Tree)

--- practice.cpp
+++ practice.cpp
@@ -6175,9 +6175,62 @@
 
 // Diameter of a Binary Tree
 
+// #include<iostream>
+// #include<vector>
+// #include<queue>
+// using namespace std;
+
+// class Node{
+// public:
+//     int data;
+//     Node* left;
+//     Node* right;
+
+//     Node(int val){
+//         data = val;
+//         left = right = NULL;
+//     }
+// };
+
+// static int idx = -1;
+// Node* buildTree(vector<int>& preorder){
+//     idx++;
+
+//     if(preorder[idx] == -1) return NULL;
+//     Node* root = new Node(preorder[idx]);
+//     root->left = buildTree(preorder);
+//     root->right = buildTree(preorder);
+//     return root;
+// }
+
+//     int ans = 0;
+//     int height(Node* root){
+//         if(root == NULL){
+//             return 0;
+//         }
+//         int leftHt = height(root->left);
+//         int rightHt = height(root->right);
+//         ans = max(ans, (rightHt+leftHt));
+//         return max(leftHt, rightHt) +1;
+//     }
+//     int diameterOfBinaryTree(Node* root) {
+//         height(root);
+//         return ans;
+//     }
+
+// int main(){
+//     vector<int> preorder = {1,2,-1,-1,3,4,-1,-1,5,-1,-1};
+//     Node* root = buildTree(preorder);
+//     cout<<"The Diameter of the Tree : "<<diameterOfBinaryTree(root)<<endl;
+//     return 0;
+// }
+
+// Top View of a Tree
+
 #include<iostream>
 #include<vector>
 #include<queue>
+#include<map>
 using namespace std;
 
 class Node{
@@ -6185,8 +6238,7 @@ public:
     int data;
     Node* left;
     Node* right;
-
-    Node(int val){
+    Node(int val){ 
         data = val;
         left = right = NULL;
     }
@@ -6203,24 +6255,36 @@ Node* buildTree(vector<int>& preorder){
     return root;
 }
 
-    int ans = 0;
-    int height(Node* root){
-        if(root == NULL){
-            return 0;
+void topView(Node* root){
+    queue<pair<Node*, int>> q;
+    map<int, int> m;
+    q.push({root,0});
+    while(q.size() > 0){
+        Node* curr = q.front().first;
+        int currHD = q.front().second;
+        q.pop();
+
+        if(m.find(currHD) == m.end()){
+            m[currHD] = curr->data;
         }
-        int leftHt = height(root->left);
-        int rightHt = height(root->right);
-        ans = max(ans, (rightHt+leftHt));
-        return max(leftHt, rightHt) +1;
+        if(curr->left != NULL){
+            q.push({curr->left, currHD-1});
+        }
+        if(curr->right != NULL){
+            q.push({curr->right, currHD+1});
+        }
     }
-    int diameterOfBinaryTree(Node* root) {
-        height(root);
-        return ans;
+
+    for(auto it : m){
+        cout<<it.second<<" ";
     }
+    cout<<endl;
+}
 
 int main(){
     vector<int> preorder = {1,2,-1,-1,3,4,-1,-1,5,-1,-1};
     Node* root = buildTree(preorder);
-    cout<<"The Diameter of the Tree : "<<diameterOfBinaryTree(root)<<endl;
+    cout<<"Top View of the Tree : ";
+    topView(root);
     return 0;
 }
