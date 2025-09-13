36ee263578ba320c1efcc03d0b58937becfb31dc
CodeWithMihran <sohail.mihran@gmail.com>
2025-09-13 23:23:32 +0530

    Practiced Leetcode Questions (Flatten Binary Tree to Linked List)

--- practice.cpp
+++ practice.cpp
@@ -6521,10 +6521,77 @@
 
 // Binary Tree Inorder Traversal
 
+// #include<iostream>
+// #include<vector>
+// #include<queue>
+// #include<map>
+// using namespace std;
+
+// class Node{
+// public:
+//     int data;
+//     Node* left;
+//     Node* right;
+//     Node(int val){ 
+//         data = val;
+//         left = right = NULL;
+//     }
+// };
+
+// static int idx = -1;
+// Node* buildTree(vector<int>& preorder){
+//     idx++;
+
+//     if(preorder[idx] == -1) return NULL;
+//     Node* root = new Node(preorder[idx]);
+//     root->left = buildTree(preorder);
+//     root->right = buildTree(preorder);
+//     return root;
+// }
+
+// vector<int> inorderTraversal(Node* root) {
+//         vector<int> ans;
+//         Node* curr = root;
+//         while(curr != NULL){
+//             if(curr->left == NULL){
+//                 ans.push_back(curr->data);
+//                 curr = curr->right;
+//             }
+//             else{
+//                 Node* IP = curr->left;
+//                 while(IP->right != NULL && IP->right != curr){
+//                     IP = IP->right;
+//                 }
+//                 if(IP->right == NULL){
+//                     IP->right = curr;
+//                     curr = curr->left;
+//                 }
+//                 else{
+//                     IP->right = NULL;
+//                     ans.push_back(curr->data);
+//                     curr = curr->right;
+//                 }
+//             }
+//         }
+//         return ans;
+//     }
+
+// int main(){
+//     vector<int> preOrder = {1,2,-1,-1,3,4,-1,-1,5,-1,-1};
+//     Node* root = buildTree(preOrder);
+//     vector<int> ans = inorderTraversal(root);
+//     cout<<"Tree in Inorder Traversal : ";
+//     for(int val : ans){
+//         cout<< val <<" ";
+//     }
+//     cout<<endl;
+//     return 0;
+// }
+
+// Flatten Binary Tree to Linked List
+
 #include<iostream>
 #include<vector>
-#include<queue>
-#include<map>
 using namespace std;
 
 class Node{
@@ -6549,40 +6616,27 @@ Node* buildTree(vector<int>& preorder){
     return root;
 }
 
-vector<int> inorderTraversal(Node* root) {
-        vector<int> ans;
-        Node* curr = root;
-        while(curr != NULL){
-            if(curr->left == NULL){
-                ans.push_back(curr->data);
-                curr = curr->right;
-            }
-            else{
-                Node* IP = curr->left;
-                while(IP->right != NULL && IP->right != curr){
-                    IP = IP->right;
-                }
-                if(IP->right == NULL){
-                    IP->right = curr;
-                    curr = curr->left;
-                }
-                else{
-                    IP->right = NULL;
-                    ans.push_back(curr->data);
-                    curr = curr->right;
-                }
-            }
-        }
-        return ans;
+Node* nextRight = NULL;
+void flatten(Node* root) {
+    if(root == NULL){
+        return;
     }
+    flatten(root->right);
+    flatten(root->left);
+    root->left = NULL;
+    root->right = nextRight;
+    nextRight = root;
+}
 
 int main(){
     vector<int> preOrder = {1,2,-1,-1,3,4,-1,-1,5,-1,-1};
     Node* root = buildTree(preOrder);
-    vector<int> ans = inorderTraversal(root);
-    cout<<"Tree in Inorder Traversal : ";
-    for(int val : ans){
-        cout<< val <<" ";
+    flatten(root);
+    cout<<"Flatten Tree (Right-Skewed) : ";
+    Node* curr = root;
+    while(curr != NULL){
+        cout<<curr->data<<" ";
+        curr = curr->right;
     }
     cout<<endl;
     return 0;
