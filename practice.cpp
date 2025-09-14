8803a371828a74bf97e06f2d7f873c751076c5dd
CodeWithMihran <sohail.mihran@gmail.com>
2025-09-14 20:09:24 +0530

    Leearning the concept to delete a Node in  Binary Search Tree

--- practice.cpp
+++ practice.cpp
@@ -6742,6 +6742,73 @@
 
 // Search in Binary Search Tree
 
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+
+// class Node{
+// public:
+//     int data;
+//     Node* right;
+//     Node* left;
+
+//     Node(int val){
+//         data = val;
+//         right = left = NULL;
+//     }
+// };
+
+// Node* insert(Node* root, int val){
+//     if(root == NULL){
+//         return new Node(val);
+//     }
+//     if(val < root->data){
+//         root->left = insert(root->left, val);
+//     }
+//     else{
+//         root->right = insert(root->right, val);
+//     }
+//     return root;
+// }
+
+// Node* buildBST(vector<int> arr){
+//     Node* root = NULL;
+//     for(int val : arr){
+//         root = insert(root, val);
+//     }
+//     return root;
+// }
+
+// bool search(Node* root, int key){
+//     if(root == NULL){
+//         return false;
+//     }
+//     if(root->data == key){
+//         return true;
+//     }
+//     if(root->data > key){
+//         return search(root->left, key);
+//     }
+//     else{
+//         return search(root->right, key);
+//     }
+// }
+
+// int main(){
+//     vector<int> arr = {3,2,1,5,4,6};
+//     Node* root = buildBST(arr);
+//     int key = 5;
+//     if(search(root,key)){
+//         cout<<"The Key is present in the Tree."<<endl;
+//     }
+//     else{
+//         cout<<"The Key is not present in the Tree."<<endl;
+//     }
+//     return 0;
+// }
+
+// Delete Node in Binary Search Tree 
+
 #include<iostream>
 #include<vector>
 using namespace std;
@@ -6779,30 +6846,64 @@ Node* buildBST(vector<int> arr){
     return root;
 }
 
-bool search(Node* root, int key){
+Node* getInorderSuccessor(Node* root){
+    while(root != NULL && root->left != NULL){
+        root = root->left;
+    }
+    return root;
+}
+
+Node* delNode(Node* root, int key){
     if(root == NULL){
-        return false;
+        return NULL;
     }
-    if(root->data == key){
-        return true;
+    if(key < root->data){
+        root->left = delNode(root->left, key);
     }
-    if(root->data > key){
-        return search(root->left, key);
+    else if(key > root->data){
+        root->right = delNode(root->right, key);
     }
     else{
-        return search(root->right, key);
+        if(root->left == NULL){
+            Node* temp = root->right;
+            delete root;
+            return temp;
+        }
+        else if(root->right == NULL){
+            Node* temp = root->left;
+            delete root;
+            return temp;
+        }
+        else{
+            Node* IS = getInorderSuccessor(root->right);
+            root->data = IS->data;
+            root->right = delNode(root->right, IS->data);
+        }
     }
+    return root;
+}
+
+void inorder(Node* root){
+    if(root == NULL){
+        return;
+    }
+    inorder(root->left);
+    cout<<root->data<<" ";
+    inorder(root->right);
 }
 
 int main(){
     vector<int> arr = {3,2,1,5,4,6};
     Node* root = buildBST(arr);
     int key = 5;
-    if(search(root,key)){
-        cout<<"The Key is present in the Tree."<<endl;
-    }
-    else{
-        cout<<"The Key is not present in the Tree."<<endl;
-    }
+    cout<<"Before : ";
+    inorder(root);
+    cout<<endl;
+
+    delNode(root, key);
+    cout<<"After : ";
+    inorder(root);
+    cout<<endl;
+    
     return 0;
 }
