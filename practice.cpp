dac938184e7b57061ee7191f637fef20172c885c
CodeWithMihran <sohail.mihran@gmail.com>
2025-09-19 18:09:07 +0530

    Learned Graph Concept and Cycle Detection in Undirected Graph by BFS

--- practice.cpp
+++ practice.cpp
@@ -7916,50 +7916,129 @@
 // }
 
 
-// Cycle Detection in Undirected Graph
+// Cycle Detection in Undirected Graph using DFS
+
+// #include<iostream>
+// #include<vector>
+// #include<list>
+// #include<queue>
+// using namespace std;
+
+// class Graph{
+//     int V;
+//     list<int> *l;
+// public:
+//     Graph(int V){
+//         this->V = V;
+//         l = new list<int>[V];
+//     }
+//     void addEdge(int u, int v){
+//         l[u].push_back(v);
+//         l[v].push_back(u);
+//     }
+    
+//     bool isCycleUndirDFS(int src, int par, vector<bool>& vis){
+//         vis[src] = true;
+//         list<int> neighbors = l[src];
+
+//         for(int v : neighbors){
+//             if(!vis[v]){
+//                 if(isCycleUndirDFS(v, src, vis)){
+//                     return true;
+//                 }
+
+//             }
+//             else if(v != par){
+//                 return true;
+//             }
+//         }
+//         return false;
+//     }
+
+//     bool isCycle(){
+//         vector<bool>vis(V, false);
+//         for(int i=0; i<V; i++){
+//             if(!vis[i]){
+//                 if(isCycleUndirDFS(i, -1, vis)){
+//                     return true;
+//                 }
+//             }
+//         }
+//         return false;
+//     }
+// };
+
+// int main(){
+//     Graph g(5);
+
+//     g.addEdge(0,1);
+//     g.addEdge(0,2);
+//     g.addEdge(0,3);
+//     g.addEdge(1,2);
+//     g.addEdge(3,4);
+
+//     if(g.isCycle()){
+//         cout<<"The Graph is Cycled."<<endl;
+//     }
+//     else{
+//         cout<<"The Graph is not Cycled."<<endl;
+//     }
+//     return 0;
+// }
+
+
+// Cycle Detection in Undirected Graph using BFS
 
 #include<iostream>
 #include<vector>
-#include<list>
 #include<queue>
+#include<list>
 using namespace std;
 
 class Graph{
     int V;
     list<int> *l;
+
 public:
     Graph(int V){
         this->V = V;
-        l = new list<int>[V];
-    }
+        l = new list<int> [V];
+    }    
+
     void addEdge(int u, int v){
         l[u].push_back(v);
         l[v].push_back(u);
     }
-    
-    bool isCycleUndirDFS(int src, int par, vector<bool>& vis){
-        vis[src] = true;
-        list<int> neighbors = l[src];
 
-        for(int v : neighbors){
-            if(!vis[v]){
-                if(isCycleUndirDFS(v, src, vis)){
+    bool isCycleUndirBFS(int src, vector<bool>& vis){
+        queue<pair<int,int>> q;
+        q.push({src, -1});
+        vis[src] = true;
+
+        while(q.size() > 0){
+            int u = q.front().first;
+            int parU = q.front().second;
+            q.pop();
+
+            list<int> neighbors = l[u];
+            for(int v : neighbors){
+                if(!vis[v]){
+                    q.push({v,u});
+                    vis[v] = true;
+                }
+                else if(v != parU){
                     return true;
                 }
-
-            }
-            else if(v != par){
-                return true;
             }
         }
         return false;
     }
 
     bool isCycle(){
-        vector<bool>vis(V, false);
+        vector<bool> vis(V, false);
         for(int i=0; i<V; i++){
             if(!vis[i]){
-                if(isCycleUndirDFS(i, -1, vis)){
+                if(isCycleUndirBFS(i, vis)){
                     return true;
                 }
             }
@@ -7981,7 +8060,7 @@ int main(){
         cout<<"The Graph is Cycled."<<endl;
     }
     else{
-        cout<<"The Graph is not Cycled."<<endl;
+        cout<<"The Graph is Not Cycled."<<endl;
     }
     return 0;
 }
